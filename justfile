58e931070fe3d10d2ca28c1f669f778a2bae4b9f
Noveren <1079148276@qq.com>
2023-12-30 14:46:26 +0800

    自定义 assembly 规则，使 xmake 可以批量转换汇编文件

--- justfile
+++
@@ -1,2 +0,0 @@
-convert:
-    arm-none-eabi-gcc -S -std=c++17 -Iinc -ICMSIS/Core -ICMSIS/Device -DSTM32F1 -DSTM32F103xB -mcpu=cortex-m3 -mthumb -Wall -Og -fdata-sections -ffunction-sections -U__VFP_FP__ ./example/blinky.cc
--- xmake.lua
+++ xmake.lua
@@ -96,3 +96,57 @@ target("blinky-ll") do
         "./STM32F1xx_LL/src/stm32f1xx_ll_exti.c"
     )
 end
+
+rule("assembly") do
+    set_extensions(".c", ".cc")
+    
+    on_build_file(function(target, sourcefile, opt)
+        -- TODO 支持语言标准设置
+        print(format("Convert to Assembly: %s", sourcefile))
+        local object_directory = path.directory(target:objectfile(sourcefile))
+        if not os.exists(object_directory) then
+            os.mkdir(object_directory)
+        end
+        local template = format(
+            "arm-none-eabi-gcc -S %s -o %s %s %s %s",
+            sourcefile,
+            path.join(object_directory, path.filename(sourcefile) .. ".s"),
+            table.concat(target:get("cxflags"), ' '),
+            "-I" .. table.concat(target:get("includedirs"), " -I"),
+            "-D" .. table.concat(target:get("defines"), " -D")
+        )
+        -- print(template)
+        os.run(template)
+    end)
+end
+
+target("assembly") do
+    set_kind("object")
+    add_rules("assembly")
+    on_load(function(target)
+        import("stm32f103c8t6").config(target, {
+            kind = "library",
+            use_CMSIS = true
+        })
+    end)
+    add_defines(
+        "HSE_VALUE=8000000",
+        "HSI_VALUE=8000000",
+        "LSE_VALUE=32768",
+        "LSI_VALUE=40000",
+        "HSE_STARTUP_TIMEOUT=100",
+        "LSE_STARTUP_TIMEOUT=100",
+        "VDD_VALUE=3300",
+        "PREFETCH_ENABLE=1",
+        "USE_FULL_LL_DRIVER"
+    )
+    add_includedirs(
+        "./inc",
+        "./STM32F1xx_LL/inc"
+    )
+    add_files(
+        "./example/blinky.cc",
+        "./example/blinky.c",
+        "./src/system_stm32f1xx.c"
+    )
+end
